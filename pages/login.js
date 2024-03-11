6d9db4d38d0f34ed3d03c843bce035c0361b5ece
AlfianKamil9 <rifqialfiansyahk@gmail.com>
2024-03-12 04:35:34 +0700

    upload admin-app- add-logi

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,10 @@
-'use client';
-
 import Image from 'next/image';
 import Logo from '../component/image/logo-image.png';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import Cookies from 'js-cookie';
-// import process from 'process';
 
-export default function Home() {
+export default function Login() {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
